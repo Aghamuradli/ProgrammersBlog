db5bc5355279b7414a5e34893d3e202dddc9ad54
BabekAga <82724876+BabekAga@users.noreply.github.com>
2022-08-22 14:41:59 +0400

    DENEME

---
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Mappings/ArticleMap.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProgrammersBlog.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Concrete.EntityFramework.Mappings
+{
+    public class ArticleMap : IEntityTypeConfiguration<Article>
+    {
+        public void Configure(EntityTypeBuilder<Article> builder)
+        {
+            builder.HasKey(a => a.Id);
+            builder.Property(a => a.Id).ValueGeneratedOnAdd();
+            builder.Property(a => a.Title).HasMaxLength(100);
+            builder.Property(a => a.Title).IsRequired(true);
+            builder.Property(a => a.Content).IsRequired();
+            builder.Property(a => a.Content).HasColumnType("NVARCHAR(MAX)");
+            builder.Property(a => a.Date).IsRequired();
+            builder.Property(a => a.SeoAuthor).IsRequired();
+            builder.Property(a => a.SeoAuthor).HasMaxLength(50);
+            builder.Property(a => a.SeoDescription).HasMaxLength(150);
+            builder.Property(a => a.SeoDescription).IsRequired();
+            builder.Property(a => a.SeoTags).IsRequired();
+            builder.Property(a => a.SeoTags).HasMaxLength(70);
+            builder.Property(a => a.ViewsCount).IsRequired();
+            builder.Property(a => a.CommentCount).IsRequired();
+            builder.Property(a => a.Thumbnail).IsRequired();
+            builder.Property(a => a.Thumbnail).HasMaxLength(250);
+            builder.Property(a => a.CreatedByName).IsRequired();
+            builder.Property(a => a.CreatedByName).HasMaxLength(50);
+            builder.Property(a => a.ModifiedByName).IsRequired();
+            builder.Property(a => a.ModifiedByName).HasMaxLength(50);
+            builder.Property(a => a.CreatedDate).IsRequired();
+            builder.Property(a => a.ModifiedDate).IsRequired();
+            builder.Property(a => a.IsActive).IsRequired();
+            builder.Property(a => a.IsDeleted).IsRequired();
+            builder.Property(a => a.Note).HasMaxLength(500);
+            builder.HasOne<Category>(a => a.Category).WithMany(c => c.Articles).HasForeignKey(a => a.CategoryId);
+            builder.HasOne<User>(a => a.User).WithMany(u => u.Articles).HasForeignKey(a => a.UserId);
+            builder.ToTable("Articles");
+
+            builder.HasData(
+                new Article
+                {
+                    Id = 1,
+                    CategoryId = 1,
+                    Title = "C# 9.0 ve .NET 5 yenilikleri ",
+                    Content = "Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.",
+                    Thumbnail = "Default.jpg",
+                    SeoDescription = "C# 9.0 ve .NET 5",
+                    SeoTags = "C# , C# 9.0 , .NET5 , .NET Framework , .NET Core",
+                    SeoAuthor = "Babek Agamuradli",
+                    Date = DateTime.Now,
+                    IsActive = true,
+                    IsDeleted = false,
+                    CreatedByName = "InitialCreate",
+                    CreatedDate = DateTime.Now,
+                    ModifiedByName = "InitialCreate",
+                    ModifiedDate = DateTime.Now,
+                    Note = "C# Blog Kategorisi",
+                    UserId = 1,
+                    ViewsCount = 100,
+                    CommentCount = 1
+                },
+                new Article
+                {
+                    Id = 2,
+                    CategoryId = 2,
+                    Title = "C++ 11.0 ve 19.0 yenilikleri ",
+                    Content = "Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir. Lorem Ipsum kullanmanın amacı, sürekli 'buraya metin gelecek, buraya metin gelecek' yazmaya kıyasla daha dengeli bir harf dağılımı sağlayarak okunurluğu artırmasıdır. Şu anda birçok masaüstü yayıncılık paketi ve web sayfa düzenleyicisi, varsayılan mıgır metinler olarak Lorem Ipsum kullanmaktadır. Ayrıca arama motorlarında 'lorem ipsum' anahtar sözcükleri ile arama yapıldığında henüz tasarım aşamasında olan çok sayıda site listelenir. Yıllar içinde, bazen kazara, bazen bilinçli olarak (örneğin mizah katılarak), çeşitli sürümleri geliştirilmiştir.",
+                    Thumbnail = "Default.jpg",
+                    SeoDescription = "C++ 11.0 ve 19.0",
+                    SeoTags = "C++ 11.0 ve 19.0",
+                    SeoAuthor = "Babek Agamuradli",
+                    Date = DateTime.Now,
+                    IsActive = true,
+                    IsDeleted = false,
+                    CreatedByName = "InitialCreate",
+                    CreatedDate = DateTime.Now,
+                    ModifiedByName = "InitialCreate",
+                    ModifiedDate = DateTime.Now,
+                    Note = "C++ Blog Kategorisi",
+                    UserId = 1,
+                    ViewsCount = 200,
+                    CommentCount = 20
+                },
+                new Article
+                {
+                    Id = 3,
+                    CategoryId = 3,
+                    Title = "JavaScript ES2019 ve ES2020 yenilikleri ",
+                    Content = "Lorem Ipsum pasajlarının birçok çeşitlemesi vardır. Ancak bunların büyük bir çoğunluğu mizah katılarak veya rastgele sözcükler eklenerek değiştirilmişlerdir. Eğer bir Lorem Ipsum pasajı kullanacaksanız, metin aralarına utandırıcı sözcükler gizlenmediğinden emin olmanız gerekir. İnternet'teki tüm Lorem Ipsum üreteçleri önceden belirlenmiş metin bloklarını yineler. Bu da, bu üreteci İnternet üzerindeki gerçek Lorem Ipsum üreteci yapar. Bu üreteç, 200'den fazla Latince sözcük ve onlara ait cümle yapılarını içeren bir sözlük kullanır. Bu nedenle, üretilen Lorem Ipsum metinleri yinelemelerden, mizahtan ve karakteristik olmayan sözcüklerden uzaktır.",
+                    Thumbnail = "Default.jpg",
+                    SeoDescription = "JavaScript ES2019 ve ES2020",
+                    SeoTags = "JavaScript ES2019 ve ES2020",
+                    SeoAuthor = "Babek Agamuradli",
+                    Date = DateTime.Now,
+                    IsActive = true,
+                    IsDeleted = false,
+                    CreatedByName = "InitialCreate",
+                    CreatedDate = DateTime.Now,
+                    ModifiedByName = "InitialCreate",
+                    ModifiedDate = DateTime.Now,
+                    Note = "JavaScript Blog Kategorisi",
+                    UserId = 1,
+                    ViewsCount = 65,
+                    CommentCount = 2,
+                }
+                );
+        }
+    }
+}
