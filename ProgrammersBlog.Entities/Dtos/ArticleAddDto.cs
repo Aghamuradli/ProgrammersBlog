f7ceb5217c51c3d70127fb33cba2937c7c07e87a
BabekAga <82724876+BabekAga@users.noreply.github.com>
2022-08-25 15:28:37 +0400

    Finished Service Layer

---
+++ ProgrammersBlog.Entities/Dtos/ArticleAddDto.cs
@@ -0,0 +1,55 @@
+﻿using ProgrammersBlog.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Entities.Dtos
+{
+    public class ArticleAddDto
+    {
+        [DisplayName("Başlıq")]
+        [Required(ErrorMessage = "{0} alanı boş keçilməməlidir.")]
+        [MaxLength(100, ErrorMessage = "{0} alanı {1} karakterdən böyük olmamalıdır.")]
+        [MinLength(5, ErrorMessage = "{0} alanı {1} karakterdən küçük olmamalıdır.")]
+        public string Title { get; set; }
+        [DisplayName("Məzmun")]
+        [Required(ErrorMessage = "{0} alanı boş keçilməməlidir.")]
+        [MinLength(20, ErrorMessage = "{0} alanı {1} karakterdən küçük olmamalıdır.")]
+        public string Content { get; set; }
+        [DisplayName("Məzmun")]
+        [Required(ErrorMessage = "{0} alanı boş keçilməməlidir.")]
+        [MaxLength(250, ErrorMessage = "{0} alanı {1} karakterdən böyük olmamalıdır.")]
+        [MinLength(5, ErrorMessage = "{0} alanı {1} karakterdən küçük olmamalıdır.")]
+        public string Thumbnail { get; set; }
+        [DisplayName("Tarix")]
+        [Required(ErrorMessage = "{0} alanı boş keçilməməlidir.")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString ="{O:dd/MM/yyyy}")]
+        public DateTime Date { get; set; }
+        [DisplayName("Sea Yazıçı")]
+        [Required(ErrorMessage = "{0} alanı boş keçilməməlidir.")]
+        [MaxLength(50, ErrorMessage = "{0} alanı {1} karakterdən böyük olmamalıdır.")]
+        [MinLength(0, ErrorMessage = "{0} alanı {1} karakterdən küçük olmamalıdır.")]
+        public string SeoAuthor { get; set; }
+        [DisplayName("Sea Açıqlama")]
+        [Required(ErrorMessage = "{0} alanı boş keçilməməlidir.")]
+        [MaxLength(150, ErrorMessage = "{0} alanı {1} karakterdən böyük olmamalıdır.")]
+        [MinLength(0, ErrorMessage = "{0} alanı {1} karakterdən küçük olmamalıdır.")]
+        public string SeoDescription { get; set; }
+        [DisplayName("Sea Etiketlər")]
+        [Required(ErrorMessage = "{0} alanı boş keçilməməlidir.")]
+        [MaxLength(150, ErrorMessage = "{0} alanı {1} karakterdən böyük olmamalıdır.")]
+        [MinLength(0, ErrorMessage = "{0} alanı {1} karakterdən küçük olmamalıdır.")]
+        public string SeoTags { get; set; }
+        [DisplayName("Kategoriya")]
+        [Required(ErrorMessage = "{0} alanı boş keçilməməlidir.")]
+        public int CategoryId { get; set; }
+        public Category Category{ get; set; }
+        [DisplayName("Aktivdirmi?")]
+        [Required(ErrorMessage = "{0} alanı boş keçilməməlidir.")]
+        public bool IsActive { get; set; }
+    }
+}
--- ProgrammersBlog.Mvc/Startup.cs
+++ ProgrammersBlog.Mvc/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProgrammersBlog.Services.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,20 +13,17 @@ namespace ProgrammersBlog.Mvc
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+       
         public void ConfigureServices(IServiceCollection services)
         {
+            services.LoadMyServices();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
